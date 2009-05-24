39add659148f4f479d7d3e7a6be404a47b44ae54
Tyson Phalp <tyson@bitwerks.net>
2009-05-24 14:27:40 +0000

    - Add new page for changing the master password. Should have been added during the last commit, but was left out accidentally.

---
+++ frontend/chgpass.php
@@ -0,0 +1,41 @@
+<?php 
+  session_cache_limiter('nocache');
+  session_start();
+
+  require_once("lib/config.php");
+  require_once("lib/common_func.php");
+
+  // test if session is ok
+  test_session();
+  $out__ = write_header_begin("Master Password Change");
+  $out__ .= write_header_jquery();
+  $out__ .= write_header_common(); 
+  $out__ .= write_header_end();
+  $out__ .= write_header_counter();
+
+  $out__ .= <<<OUT
+  
+    <form method="post" action="$FRM_ACTION">
+      <input type="hidden" id="action" name="action" value="changepw" />
+      <div class="frm-msg"></div>
+      <center>
+      <table class="action-table" summary="change master pw">
+      <tr><th colspan="2">w3pw Master Password Change</th></tr>
+      <tr><td class="odd">Old Master Password: </td><td class="even"><input type="password" id="pw" name="pw" size="20" /> <span id="pw-msg" class="frm-msg">*</span></td></tr>
+      <tr><td class="odd">New Master Password: </td><td class="even"><input type="password" id="newpw" name="newpw" size="20" /> <span id="newpw-msg" class="frm-msg">*</span></td></tr>
+      <tr><td class="odd">Confirm New Master Password: </td><td class="even"><input type="password" id="confirm" name="confirm" size="20" /> <span id="confirm-msg" class="frm-msg">*</span></td></tr>
+      </table>
+      <input type="submit" id="btn" value="Change Now" />
+OUT;
+
+  $out__ .= write_footer_main_link("without changing.");
+  $out__ .= "</center></form>";
+  $out__ .= write_footer_onload('$("#pw").focus();
+    /*$("#btn").click(function(){
+      return checkChangePW();
+    });*/');
+  $out__ .= write_footer_timeout_init();
+  $out__ .= write_footer_common();  
+
+  echo $out__;
+?>

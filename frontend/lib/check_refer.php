67c3638fb15df86efc7d2a27cd511cdbc3a5e348
Tyson Phalp <tyson@bitwerks.net>
2009-08-03 04:31:02 +0000

    - Remove frontend/lib/check_refer.php, as it is not needed anymore. Same functionality done in a common function. - Added additional comments to frontend/lib/config.default.php.

--- frontend/lib/check_refer.php
+++
@@ -1,5 +0,0 @@
-<?php
-  if(isset($_SERVER['HTTP_REFERER']) && !eregi(BASE_DOMAIN, $_SERVER['HTTP_REFERER'])) {
-    go_home();
-  }
-?>
--- frontend/lib/config.default.php
+++ frontend/lib/config.default.php
@@ -30,17 +30,18 @@ define("TIMEOUT", 120);
 define("TIMEOUT_SHOW", 30);
 
 //Menu
-define("MENU_ITEMS_TEXT", "main list|insert|import|export|change master pw|logout");
-define("MENU_ITEMS_URLS", "main.php|insert.php|import.php|export.php|chgpass.php|logout.php");
-define("MENU_SHOW_MAIN", "0 1 2 3 5");
-define("MENU_SHOW_FOOT", "0 1 2 3 4 5");
+define("MENU_ITEMS_TEXT", "main list|insert|import|export|change master pw|logout");  // The text that shows on both menus (header and footer)
+define("MENU_ITEMS_URLS", "main.php|insert.php|import.php|export.php|chgpass.php|logout.php");  // The links that correspond to the text above.
+// The following two constants represent the menu items above. If the index is listed, then it will show up in the respective menu.
+define("MENU_SHOW_MAIN", "0 1 2 3 5");  // Header menu inclusion list
+define("MENU_SHOW_FOOT", "0 1 2 3 4 5");  // Footer menu inclusion list
 
 // Misc
-define("SYS_NAME", "w3pw"); // System name that you would like to use;
-define("BASE_DOMAIN", "host.yourdomain.com");  // Used when detecting the referrer;
-define("TMP_IMPORT_FILE", "w3pw.csv");  // Temp file names;
-define("GROUP_BY", "ALPHA");  // Used for grouping
-define("TOP_LINK", "{ top }");
+define("SYS_NAME", "w3pw"); // System name that you would like to use. Will show anywhere "w3pw" normally is displayed on the web interface.
+define("BASE_DOMAIN", "host.yourdomain.com");  // Used when detecting the referrer, for added security
+define("TMP_IMPORT_FILE", "w3pw.csv");  // Temp file names
+define("GROUP_BY", "ALPHA");  // Used for grouping. ALPHA give you grouping, and an empty string gives you the regular list, with no grouping.
+define("TOP_LINK", "{ top }");  // This is the link that is used with grouping to take you to the top of the page. Change this to whatever you want.
 define("HEADER_DEFAULT", '<tr class="header"><td class="first">Entry Name</td><td>Host/URL</td><td class="mt1">&nbsp;</td><td class="mt1">&nbsp;</td><td class="mt2">&nbsp;</td></tr>');
 define("HEADER_HIDDEN", '<tr class="invis"><th class="first">&nbsp;</th><th>&nbsp;</th><th class="mt1">&nbsp;</th><th class="mt1">&nbsp;</th><th class="mt2">&nbsp;</th></tr>');
 define("CSV_DELIM", ";");

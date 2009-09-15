c70d97655be7e858671800b901193469bc18c822
Tyson Phalp <tyson@bitwerks.net>
2009-09-15 20:43:10 +0000

    - Cleaned up /frontend/lib/process_action.php. There were a couple of redundant conditional checks. - Converted /frontend/lib/process_login.php to use the db class. Thought this had already been done, but was overlooked, apparently. Trimmed ~20 lines of code doing this.

--- frontend/lib/process_login.php
+++ frontend/lib/process_login.php
@@ -6,74 +6,53 @@
   require_once("common_func.php");  
   check_referrer(BASE_DOMAIN);
 
-  // session active?
-  if (!isset($_SESSION['logged_in'])){
-
-      // Require POST
-    if ($_SERVER['REQUEST_METHOD'] !== 'POST') { go_to_url('../' . PAGE_LOGIN); }
-
-    // no session active - check pw
-    if ($conn = mysql_connect(DB_HOST, DB_USER, DB_PASS))	{
-
-      if (mysql_select_db(DB_NAME, $conn)) {
-        $cleartext_pw = "";
-
-        // encrypt the pw given at logon
-        if (isset($_POST['password'])) {
-          $cleartext_pw = $_POST['password'];
-        }
-
-        $crypt_pw = sha1($cleartext_pw);
-
-        // check pw
-        $list = mysql_query ("SELECT version, pw FROM main");
-        $entries = mysql_fetch_object($list);
-        $db_pw=$entries->pw;
-
-        if ($crypt_pw == $db_pw)
-        {
-          // password match - proceed
-          $_SESSION['logged_in'] = 1;
-          $_SESSION['key'] = md5("%dJ9&".strtolower($cleartext_pw)."(/&k.=".strtoupper($cleartext_pw)."1x&%");
-          
-          // delete cleartext pw in memory
-          unset($cleartext_pw);
-          $_SESSION['version']=$entries->version;
-
-          //Forward to main page
-          if (isset($_GET["upgrade"])) {
-            go_to_url("../_upgrade.php");
-          } else {
-            go_to_url("../" . PAGE_MAIN);
-          }
-        }
-        else
-        {      
-          session_unset();
-          session_destroy();
-          $sysmsg__ = '<b>Invalid credentials</b>....Please <a href="index.php">try again</a>.';
-          show_sys_msg($sysmsg__);
-        }
-      }
-      else
-      {
-        // can't connect to database
-        session_unset();
-        session_destroy();	
-        $sysmsg__ = '<br />Ooops - <b>Can\'t connect to the database</b>....Please <a href="index.php">try again</a>.';
-        show_sys_msg($sysmsg__);
-      }
-      mysql_close($conn);
-    }
-    else
-    {
-      // can't connect to the server
-      session_unset();
-      session_destroy();
-      $sysmsg__ = '<br />Ooops - <b>Can\'t connect to the database-server</b>...Please <a href="index.php">try again</a>.';
-      show_sys_msg($sysmsg__);
-    }
-  } else {
-    go_to_url("../" . PAGE_MAIN);
+  // Session is active
+  if (test_session(TRUE)) {
+    go_to_url(PAGE_MAIN);
   }
+
+  // Require POST
+  if ($_SERVER['REQUEST_METHOD'] !== 'POST') { go_to_url('../' . PAGE_LOGIN); }
+
+  // Set db object
+  $db = get_db_conn();
+  $cleartext_pw = "";
+  
+  // encrypt the pw given at logon
+  if (isset($_POST['password'])) {
+    $cleartext_pw = $_POST['password'];
+  }
+
+  $crypt_pw = sha1($cleartext_pw);
+
+  // check pw
+  $entries = $db->out_row_object("SELECT version, pw FROM main");
+  $db_pw = $entries->pw;
+  
+  if ($crypt_pw == $db_pw) {
+    // passwords match - proceed
+    $_SESSION['logged_in'] = 1;
+    $_SESSION['key'] = md5("%dJ9&" . strtolower($cleartext_pw) . "(/&k.=" . strtoupper($cleartext_pw) . "1x&%");
+    
+    // delete cleartext pw in memory
+    unset($cleartext_pw);
+    $_SESSION['version'] = $entries->version;
+
+    unset($entries, $db);
+    
+    //Forward to main page
+    if (isset($_GET["upgrade"])) {
+      go_to_url("../_upgrade.php");
+    } else {
+      go_to_url("../" . PAGE_MAIN);
+    }
+
+  } else {      
+    unset($db);
+    session_unset();
+    session_destroy();
+    $sysmsg__ = '<b>Invalid credentials</b>....Please <a href="' . PAGE_LOGIN . '">try again</a>.';
+    show_sys_msg($sysmsg__);
+  }
+  
 ?>
